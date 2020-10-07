f4aee8004715f97d45f604726f340ae080cef5dd
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-07 16:31:20 -0500

    fix: env file added for connect string protection

---
+++ Minerva/Minerva/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva
+{
+    public static class Settings
+    {
+        static Settings()
+        {
+            DotNetEnv.Env.Load();
+        }
+
+        public static string CONNECT_STRING => DotNetEnv.Env.GetString("CONNECT_STRING");
+    }
+}
